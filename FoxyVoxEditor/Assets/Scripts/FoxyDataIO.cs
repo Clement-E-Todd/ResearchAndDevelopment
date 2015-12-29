4fadc3d32446df584d08c391679b8ca7c88db1c8
Clement-E-Todd <clem.e.todd@gmail.com>
2015-12-29 10:30:42 -0500

    [FoxyVox Editor] Preliminary work on data input and output

---
+++ FoxyVoxEditor/Assets/Scripts/FoxyDataIO.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class FoxyDataIO
+{
+	static List<char> streamData;
+
+	enum StreamStatus
+	{
+		Closed,
+		Writing,
+		Reading
+	}
+
+	static StreamStatus streamStatus = StreamStatus.Closed;
+	
+	public static void StartWriting()
+	{
+		if (streamStatus == StreamStatus.Closed)
+		{
+			streamStatus = StreamStatus.Writing;
+			
+			streamData = new List<char>();
+		}
+		else
+		{
+			Debug.LogError("Cannot start writing; IO stream is already open with status of \"" + streamStatus + "\"!");
+		}
+	}
+	
+	public static void StartReading(char[] data)
+	{
+		if (streamStatus == StreamStatus.Closed)
+		{
+			streamStatus = StreamStatus.Writing;
+			
+			streamData = new List<char>(data);
+		}
+		else
+		{
+			Debug.LogError("Cannot start reading; IO stream is already open with status of \"" + streamStatus + "\"!");
+		}
+	}
+	
+	public static void WriteData(object data, int maxBits)
+	{
+		
+	}
+}
