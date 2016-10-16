fc67e293986c80469799e1766792597ce805a424
Clement-E-Todd <clem.e.todd@gmail.com>
2016-10-16 17:00:49 -0400

    [HexTerrain] Created a function to determine which hex a local or world position is inside.

--- HexTerrain/Assets/Scripts/HexTerrain.cs
+++ HexTerrain/Assets/Scripts/HexTerrain.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEditor;
 using System.Collections.Generic;
 
 public abstract class HexTerrain : MonoBehaviour
@@ -18,5 +19,37 @@ public abstract class HexTerrain : MonoBehaviour
         Vector3 xDirection = HexHelper.GetEdgeCenterOffset(HexEdge.SouthEast) * 2;
         Vector3 yDirection = HexHelper.GetEdgeCenterOffset(HexEdge.NorthEast) * 2;
         return (xDirection * coord.x) + (yDirection * coord.y);
+    }
+
+    public HexGrid.Coord GetCoordForLocalPosition(Vector3 localPosition)
+    {
+        Vector3 xDirection = HexHelper.GetEdgeCenterOffset(HexEdge.SouthEast) * 2;
+        Vector3 yDirection = HexHelper.GetEdgeCenterOffset(HexEdge.NorthEast) * 2;
+
+        Ray xRay = new Ray(transform.up * Vector3.Dot(transform.up, localPosition), xDirection.normalized);
+        Plane yPlane = new Plane(Vector3.Cross(yDirection.normalized, transform.up), localPosition);
+
+        float xDistance = 0f;
+        yPlane.Raycast(xRay, out xDistance);
+        Vector3 interestionPoint = xRay.GetPoint(xDistance);
+
+        Vector2 xyDistances = new Vector2(
+            Vector3.Distance(xRay.origin, interestionPoint) / xDirection.magnitude,
+            Vector3.Distance(interestionPoint, localPosition) / yDirection.magnitude);
+
+        if (localPosition.x < 0)
+            xyDistances.x *= -1f;
+
+        if (localPosition.z < 0)
+            xyDistances.y *= -1f;
+
+        Debug.Log(xyDistances);
+
+        return new HexGrid.Coord((int)Mathf.Round(xyDistances.x), (int)Mathf.Round(xyDistances.y));
+    }
+
+    public HexGrid.Coord GetCoordForWorldPosition(Vector3 worldPosition)
+    {
+        return GetCoordForLocalPosition(transform.InverseTransformPoint(worldPosition));
     }
 }
--- HexTerrain/Assets/Scripts/HexTerrainEditor.cs
+++ HexTerrain/Assets/Scripts/HexTerrainEditor.cs
@@ -5,13 +5,96 @@ using System.Collections.Generic;
 [CustomEditor(typeof(HexTerrainEditable))]
 public class HexTerrainEditor : Editor
 {
+    const float distanceToHorizonalPlane = 10f;
+
+    enum State
+    {
+        None,
+        ChooseHorizontalPosition,
+        ChooseTopHeight,
+        ChooseBottomHeight
+    }
+    State currentState = State.None;
+
     public void OnSceneGUI()
     {
-        Handles.BeginGUI();
-        
-        if (GUILayout.Button("Press Me"))
-            Debug.Log("Hurro!");
+        HexTerrainEditable selectedTerrain = Selection.activeGameObject ? Selection.activeGameObject.GetComponent<HexTerrainEditable>() : null;
 
+        if (!selectedTerrain)
+        {
+            return;
+        }
+
+        switch (currentState)
+        {
+            case State.None:
+                None(selectedTerrain);
+                break;
+
+            case State.ChooseHorizontalPosition:
+                ChooseHorizontalPosition(selectedTerrain);
+                break;
+
+            case State.ChooseTopHeight:
+                ChooseTopHeight(selectedTerrain);
+                break;
+
+            case State.ChooseBottomHeight:
+                ChooseBottomHeight(selectedTerrain);
+                break;
+        }
+
+        HandleUtility.Repaint();
+    }
+
+    void None(HexTerrainEditable terrain)
+    {
+        Handles.BeginGUI();
+        GUILayout.BeginArea(new Rect(
+            Screen.width * 0.05f, Screen.height * 0.875f,
+            Screen.width * 0.9f, Screen.height * 0.125f));
+
+        if (GUILayout.Button("Create New Hex Pillars"))
+            currentState = State.ChooseHorizontalPosition;
+
+        GUILayout.EndArea();
         Handles.EndGUI();
+    }
+
+    void ChooseHorizontalPosition(HexTerrainEditable terrain)
+    {
+        if (!Camera.current)
+            return;
+
+        Vector3 planeTargetPosition = Camera.current.transform.position + Camera.current.transform.forward * distanceToHorizonalPlane;
+
+        Plane plane = new Plane(terrain.transform.up, planeTargetPosition);
+        Ray ray = Camera.current.ScreenPointToRay(new Vector2(Event.current.mousePosition.x, Screen.height - Event.current.mousePosition.y));
+
+        float rayDistance;
+        if (plane.Raycast(ray, out rayDistance))
+        {
+            Vector3 cursorWorldPosition = ray.GetPoint(rayDistance);
+
+            Plane topPlane = new Plane(terrain.transform.up, terrain.transform.position + terrain.transform.up * terrain.maxHeight);
+            Plane bottomPlane = new Plane(terrain.transform.up, terrain.transform.position + terrain.transform.up * terrain.minHeight);
+
+            Handles.DrawLine(
+                cursorWorldPosition - topPlane.normal * topPlane.GetDistanceToPoint(cursorWorldPosition),
+                cursorWorldPosition - bottomPlane.normal * bottomPlane.GetDistanceToPoint(cursorWorldPosition)
+                );
+
+            HexGrid.Coord coord = terrain.GetCoordForWorldPosition(cursorWorldPosition);
+        }
+    }
+
+    void ChooseTopHeight(HexTerrainEditable terrain)
+    {
+        currentState = State.ChooseBottomHeight;
+    }
+
+    void ChooseBottomHeight(HexTerrainEditable terrain)
+    {
+        currentState = State.None;
     }
 }
