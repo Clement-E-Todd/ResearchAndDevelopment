7be39e3b98bc5cc9dd6c7942bdfffe2320e15ba7
Unknown <clem.e.todd@gmail.com>
2017-11-17 13:04:17 -0500

    Started 1.4 (skipped 1.3), next section is 1.4.4 (Averaging FPS)

---
+++ Assets/1 Basics/4 Frames Per Second/NucleonSpawner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class NucleonSpawner : MonoBehaviour
+{
+	public float timeBetweenSpawns;
+
+	public float spawnDistance;
+
+	public float spawnsPerBurst;
+
+	public Nucleon[] nucleonPrefabs;
+
+	float timeSinceLastSpawn;
+
+	private void FixedUpdate()
+	{
+		timeSinceLastSpawn += Time.deltaTime;
+		if (timeSinceLastSpawn >= timeBetweenSpawns)
+		{
+			timeSinceLastSpawn -= timeBetweenSpawns;
+			SpawnNucleon();
+		}
+	}
+
+	private void SpawnNucleon()
+	{
+		for (int i = 0; i < spawnsPerBurst; i++)
+		{
+			Nucleon prefab = nucleonPrefabs[Random.Range(0, nucleonPrefabs.Length)];
+			Nucleon spawn = Instantiate<Nucleon>(prefab);
+			spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+		}
+	}
+}
