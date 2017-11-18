3a69cf6254f447ecbad043f19f5bda578bc8e6cf
Unknown <clem.e.todd@gmail.com>
2017-11-17 19:18:45 -0500

    Did first part of 1.5 (object pooling)

---
+++ Assets/1 Basics/5 Object Pools/Stuff.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Stuff : MonoBehaviour
+{
+	public Rigidbody Body { get; private set; }
+
+	void Awake()
+	{
+		Body = GetComponent<Rigidbody>();
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Kill Zone"))
+		{
+			Destroy(gameObject);
+		}
+	}
+}
