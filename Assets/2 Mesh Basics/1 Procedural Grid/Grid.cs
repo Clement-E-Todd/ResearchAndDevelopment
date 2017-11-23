cfd4dca16341c6d089d9c359b6067bdecac0e950
Unknown <clem.e.todd@gmail.com>
2017-11-23 13:04:28 -0500

    Continued 2.1 up to beginning of part 4

--- Assets/2 Mesh Basics/1 Procedural Grid/Grid.cs
+++ Assets/2 Mesh Basics/1 Procedural Grid/Grid.cs
@@ -8,6 +8,8 @@ public class Grid : MonoBehaviour
 
 	private Vector3[] vertices;
 
+	private Mesh mesh;
+
 	private void Awake()
 	{
 		StartCoroutine(Generate());
@@ -15,8 +17,12 @@ public class Grid : MonoBehaviour
 
 	private IEnumerator Generate()
 	{
-		WaitForSeconds wait = new WaitForSeconds(0.05f);
+		WaitForSeconds wait = new WaitForSeconds(0.01f);
 
+		GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+		mesh.name = "Procedural Grid";
+
+		// Define the vertices
 		vertices = new Vector3[(xSize + 1) * (ySize + 1)];
 
 		for (int i = 0, y = 0; y <= ySize; y++)
@@ -25,6 +31,31 @@ public class Grid : MonoBehaviour
 			{
 				vertices[i] = new Vector3(x, y);
 
+				yield return wait;
+			}
+		}
+
+		mesh.vertices = vertices;
+
+		// Define the triangles
+		int[] triangles = new int[xSize * ySize * 6];
+
+		for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
+		{
+			for (int x = 0; x < xSize; x++, ti += 6, vi++)
+			{
+				triangles[ti] = vi;
+				triangles[ti + 1] = vi + xSize + 1;
+				triangles[ti + 2] = vi + 1;
+
+				mesh.triangles = triangles;
+				yield return wait;
+
+				triangles[ti + 3] = vi + 1;
+				triangles[ti + 4] = vi + xSize + 1;
+				triangles[ti + 5] = vi + xSize + 2;
+
+				mesh.triangles = triangles;
 				yield return wait;
 			}
 		}
